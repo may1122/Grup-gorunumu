b58e23b507f6b728ed827cecdae682475ada7797
may11224 <39835412+may1122@users.noreply.github.com>
2026-03-22 09:53:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("3 / 6 / 9 Ay Grup Karşılaştırma")
+st.title("Nöbet Sayısı Görünümü")
 
 uploaded_file = st.file_uploader(
     "Excel yükle",
@@ -13,29 +13,41 @@ if uploaded_file is not None:
 
     df = pd.read_excel(uploaded_file)
 
-    # SÜTUN TEMİZLE
     df.columns = df.columns.str.strip()
 
     st.write("Sütunlar:", df.columns)
 
+    # ALT GRUP SEÇ
     gruplar = df["Grup"].unique()
 
     secilen_grup = st.selectbox(
-        "Grup seç",
+        "Alt grup seç",
         gruplar
     )
 
+    # SÜRE SEÇ
+    sure = st.radio(
+        "Süre seç",
+        ["3 ay", "6 ay", "9 ay"],
+        horizontal=True
+    )
+
+    # GRUP FİLTRE
     df_grup = df[df["Grup"] == secilen_grup]
 
-    toplam = df_grup[
-        ["3 ay", "6 ay", "9 ay"]
-    ].sum()
+    st.subheader(f"{secilen_grup} - {sure}")
 
+    # Grafik
     fig, ax = plt.subplots()
 
     ax.bar(
-        ["3 ay", "6 ay", "9 ay"],
-        toplam.values
+        df_grup["Eczane"],
+        df_grup[sure]
     )
+
+    ax.set_ylabel("Nöbet sayısı")
+    ax.set_xlabel("Eczane")
+
+    plt.xticks(rotation=45)
 
     st.pyplot(fig)
