d1dedd3c1e822ff12c6269167cf8541e1938116c
may11224 <39835412+may1122@users.noreply.github.com>
2026-03-22 09:56:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("Nöbet Sayısı Görünümü")
+st.set_page_config(
+    page_title="Nöbet Analiz",
+    layout="wide"
+)
+
+st.title("📊 Nöbet Sayısı Görünümü")
 
 uploaded_file = st.file_uploader(
-    "Excel yükle",
+    "Excel dosyası yükle",
     type=["xlsx"]
 )
 
@@ -15,30 +20,33 @@ if uploaded_file is not None:
 
     df.columns = df.columns.str.strip()
 
-    st.write("Sütunlar:", df.columns)
+    col1, col2 = st.columns(2)
 
-    # ALT GRUP SEÇ
-    gruplar = df["Grup"].unique()
+    with col1:
+        gruplar = df["Grup"].unique()
 
-    secilen_grup = st.selectbox(
-        "Alt grup seç",
-        gruplar
-    )
+        secilen_grup = st.selectbox(
+            "Alt grup seç",
+            gruplar
+        )
 
-    # SÜRE SEÇ
-    sure = st.radio(
-        "Süre seç",
-        ["3 ay", "6 ay", "9 ay"],
-        horizontal=True
-    )
+    with col2:
 
-    # GRUP FİLTRE
+        sure = st.radio(
+            "Süre",
+            ["3 ay", "6 ay", "9 ay"],
+            horizontal=True
+        )
+
     df_grup = df[df["Grup"] == secilen_grup]
 
-    st.subheader(f"{secilen_grup} - {sure}")
+    st.markdown("---")
 
-    # Grafik
-    fig, ax = plt.subplots()
+    st.subheader(
+        f"{secilen_grup} grubu - {sure}"
+    )
+
+    fig, ax = plt.subplots(figsize=(10,5))
 
     ax.bar(
         df_grup["Eczane"],
@@ -46,8 +54,7 @@ if uploaded_file is not None:
     )
 
     ax.set_ylabel("Nöbet sayısı")
-    ax.set_xlabel("Eczane")
 
     plt.xticks(rotation=45)
 
-    st.pyplot(fig)
+    st.pyplot(fig, use_container_width=True)
